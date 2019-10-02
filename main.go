98dab00d48a9e87e373e551be40119d54a38e5af
FritzJo <fritzjo-git@mailbox.org>
2019-10-02 15:17:25 +0200

    initial code for reading from pacman

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "bytes"
+    "fmt"
+    "os"
+    "os/exec"
+    "strings"
+)
+
+func main() {
+    cmd := exec.Command("pacman", "-Q")
+    cmdOutput := &bytes.Buffer{}
+    cmd.Stdout = cmdOutput
+    printCommand(cmd)
+    err := cmd.Run()
+    printError(err)
+    printOutput(cmdOutput.Bytes())
+}
+
+
+func printCommand(cmd *exec.Cmd) {
+    fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+
+func printError(err error) {
+    if err != nil {
+        os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+    }
+}
+
+
+func printOutput(outs []byte) {
+    if len(outs) > 0 {
+        fmt.Printf("==> Output: %s\n", string(outs))
+    }
+}

6348529933f7a9fef9ac6dadbb9b8f4f88aa1073
FritzJo <fritzjo-git@mailbox.org>
2019-10-05 19:45:35 +0200

    gofmt formating

--- main.go
+++ main.go
@@ -1,84 +1,81 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "os/exec"
-    "strings"
-    "encoding/json"
-    "bufio"
-    "log"
-    "net/http"
-    "time"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+	"time"
 )
 
-
 type vulnerability struct {
-    Name        string   `json:"name"`
-    Packages    []string `json:"packages"`
-    Severity    string   `json:"severity"`
-    Type        string   `json:"type"`
-    Affected    string   `json:"affected"`
-    Fixed       string   `json:"fixed"`
-    Ticket      string   `json:"ticket"`
-    Issues      []string `json:"issues"`
-    Advisories  []string `json:"advisories"`
+	Name       string   `json:"name"`
+	Packages   []string `json:"packages"`
+	Severity   string   `json:"severity"`
+	Type       string   `json:"type"`
+	Affected   string   `json:"affected"`
+	Fixed      string   `json:"fixed"`
+	Ticket     string   `json:"ticket"`
+	Issues     []string `json:"issues"`
+	Advisories []string `json:"advisories"`
 }
-
 
 func main() {
-    vulnerabilities := getVulnerabilities()
-    cmd := exec.Command("pacman", "-Q")
-    cmdOutput := &bytes.Buffer{}
-    cmd.Stdout = cmdOutput
-    err := cmd.Run()
+	vulnerabilities := getVulnerabilities()
+	cmd := exec.Command("pacman", "-Q")
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+	err := cmd.Run()
 
-    if err != nil {
-        fmt.Println(err)
-    }
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    if len(cmdOutput.Bytes())>0 {
-        x := string(cmdOutput.Bytes())
-        scanner := bufio.NewScanner(strings.NewReader(x))
-        for scanner.Scan() {
-            for _, vuln := range vulnerabilities {
-                for _, pack := range vuln.Packages {
-                    if strings.Contains(scanner.Text(), pack) {
-                        fmt.Println(vuln.Severity + ": " + scanner.Text())
-                    }
-                }
-            }
-        }
-    }
+	if len(cmdOutput.Bytes()) > 0 {
+		x := string(cmdOutput.Bytes())
+		scanner := bufio.NewScanner(strings.NewReader(x))
+		for scanner.Scan() {
+			for _, vuln := range vulnerabilities {
+				for _, pack := range vuln.Packages {
+					if strings.Contains(scanner.Text(), pack) {
+						fmt.Println(vuln.Severity + ": " + scanner.Text())
+					}
+				}
+			}
+		}
+	}
 }
 
-
 func getVulnerabilities() []vulnerability {
-    url := "https://security.archlinux.org/vulnerable.json"
-    vulnClient := http.Client{
-        Timeout: time.Second * 2, // Maximum of 2 secs
-    }
+	url := "https://security.archlinux.org/vulnerable.json"
+	vulnClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
 
-    req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    res, getErr := vulnClient.Do(req)
-    if getErr != nil {
-        log.Fatal(getErr)
-    }
+	res, getErr := vulnClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
 
-    if err != nil {
-        fmt.Println(err)
-    }
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    result := make([]vulnerability, 0)
-    decoder := json.NewDecoder(res.Body)
-    err = decoder.Decode(&result)
-    if err != nil {
-        fmt.Println(err)
-    }
+	result := make([]vulnerability, 0)
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&result)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    return result
+	return result
 }
